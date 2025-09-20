600d605fab5b33597fa9f3884ece705bec5479cd
CHABCHOUB SALIM p2313362 <salim.chabchoub@etu.univ-lyon1.fr>
2025-09-20 23:35:29 +0200

    partie 4

--- QNN.py
+++ QNN.py
@@ -12,6 +12,14 @@ class QNN(nn.Module):
         super(QNN, self).__init__()
         
         "*** TODO ***"
+        self.dim_entree = dim_entree
+        self.dim_sortie = dim_sortie
+        self.fc1 = nn.Linear(dim_entree, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, dim_sortie)
+        
+
         
     def forward(self, etat: np.ndarray) -> torch.Tensor :
         """Forward pass"""
@@ -20,7 +28,8 @@ class QNN(nn.Module):
             etat = torch.tensor(etat, dtype=torch.float)
             
         "*** TODO ***"
-        
-        return etat
-
+        etat = F.relu(self.fc1(etat))
+        etat = F.relu(self.fc2(etat))
+        etat = F.relu(self.fc3(etat))
+        return self.fc4(etat)
 
--- agentsimple.py
+++ agentsimple.py
@@ -7,16 +7,24 @@ from QNN import QNN
 class AgentSimple():
     """Agent qui utilise la prédiction de son réseau de neurones pour choisir ses actions selon une stratégie d’exploration (pas d'apprentissage)."""
 
-    def __init__(self):
+    def __init__(self,dim_etat:int = 8, dim_action:int = 4):
         """
 
         """
+        self.dim_etat = dim_etat
+        self.dim_action = dim_action
+        self.qnn = QNN(dim_etat, dim_action)
 
     def action_egreedy(self, etat : np.ndarray , eps: float = 0.0) -> int:
         """
             eps: probabilité d'exploration
         """
-        
+        if random.random() > eps:
+            with torch.no_grad():
+                q_values = self.qnn.forward(etat)
+                return torch.argmax(q_values).item()
+        else:
+            return random.randint(0, self.dim_action - 1)
         return 0
     
         
